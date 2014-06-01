89ba3ba21ffa76c40b233f5bc9a01b13d351ac35
edrickwong <wong.edrick@gmail.com>
2014-06-01 16:48:58 -0400

    did some stuff to break the website ):

--- js/main.js
+++ js/main.js
@@ -19,3 +19,40 @@ $("#copy").click(function() {
     $("#copyMenu").fadeIn();
 });
     
+	
+$("#ex6").slider();
+$("#ex6").on('slide', function(slideEvt) {
+	$("#ex6SliderVal").text(slideEvt.value);
+});
+
+$("#ex7").slider();
+$("#ex7").on('slide', function(slideEvt) {
+	$("#ex7SliderVal").text(slideEvt.value);
+});
+
+$("#ex8").slider();
+$("#ex8").on('slide', function(slideEvt) {
+	$("#ex8SliderVal").text(slideEvt.value);
+});
+
+$('.btn-toggle').click(function() {
+    $(this).find('.btn').toggleClass('active');  
+    
+    if ($(this).find('.btn-primary').size()>0) {
+    	$(this).find('.btn').toggleClass('btn-primary');
+    }
+    if ($(this).find('.btn-danger').size()>0) {
+    	$(this).find('.btn').toggleClass('btn-danger');
+    }
+    if ($(this).find('.btn-success').size()>0) {
+    	$(this).find('.btn').toggleClass('btn-success');
+    }
+    if ($(this).find('.btn-info').size()>0) {
+    	$(this).find('.btn').toggleClass('btn-info');
+    }
+    
+    $(this).find('.btn').toggleClass('btn-default');
+       
+});
+
+
