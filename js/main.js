f6873f60963770843e1049d6c1823473393b58eb
RaunaqSuri <raunaq.suri@hotmail.com>
2014-06-01 20:36:42 -0400

    Now displays progress bar after submit is pressed. Also then goes back to the main menu

--- js/main.js
+++ js/main.js
@@ -22,12 +22,20 @@ $("#copy").click(function() {
     
 $("#submit").click(function(){
 	$("#copyMenu").fadeOut();
+    $("#progressPage").fadeIn().delay(600);
+    
 	progress(100,$("#progressBar"));
+    window.setTimeout(showDefault, 5000);
 });
 
 function progress(percent, $element) {
     var progressBarWidth = percent * $element.width() / 100;
-    $element.find('div').$("animate({ width: progressBarWidth }, 3000).html(percent + "%&nbsp;");
+    $element.find('div').animate({ width: progressBarWidth }, 3000).html(percent + "%&nbsp;");
+}
+
+function showDefault(){
+    $("#progressPage").fadeOut();
+    $("#mainMenu").fadeIn();
 }
 
 
