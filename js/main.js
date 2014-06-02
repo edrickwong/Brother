d397a54f630a27963271e0a93211950f07967637
edrickwong <wong.edrick@gmail.com>
2014-06-01 20:13:08 -0400

    copy menu

--- js/main.js
+++ js/main.js
@@ -17,48 +17,17 @@ $(document).ready(function() {
 $("#copy").click(function() {
     $("#mainMenu").fadeOut();
     $("#copyMenu").fadeIn();
-    progress(100,$("#progressBar"));
+    
 });
     
-	
-$("#ex6").slider();
-$("#ex6").on('slide', function(slideEvt) {
-	$("#ex6SliderVal").text(slideEvt.value);
-});
-
-$("#ex7").slider();
-$("#ex7").on('slide', function(slideEvt) {
-	$("#ex7SliderVal").text(slideEvt.value);
-});
-
-$("#ex8").slider();
-$("#ex8").on('slide', function(slideEvt) {
-	$("#ex8SliderVal").text(slideEvt.value);
-});
-
-$('.btn-toggle').click(function() {
-    $(this).find('.btn').toggleClass('active');  
-    
-    if ($(this).find('.btn-primary').size()>0) {
-    	$(this).find('.btn').toggleClass('btn-primary');
-    }
-    if ($(this).find('.btn-danger').size()>0) {
-    	$(this).find('.btn').toggleClass('btn-danger');
-    }
-    if ($(this).find('.btn-success').size()>0) {
-    	$(this).find('.btn').toggleClass('btn-success');
-    }
-    if ($(this).find('.btn-info').size()>0) {
-    	$(this).find('.btn').toggleClass('btn-info');
-    }
-    
-    $(this).find('.btn').toggleClass('btn-default');
-       
+$("#submit").click(function(){
+	$("#copyMenu").fadeOut();
+	progress(100,$("#progressBar"));
 });
 
 function progress(percent, $element) {
     var progressBarWidth = percent * $element.width() / 100;
-    $element.find('div').animate({ width: progressBarWidth }, 3000).html(percent + "%&nbsp;");
+    $element.find('div').$("animate({ width: progressBarWidth }, 3000).html(percent + "%&nbsp;");
 }
 
 
